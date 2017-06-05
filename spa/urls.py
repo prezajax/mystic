791708f02b3d2b8771222b1189d9a0ae786a1660
Andrew Jackson <andrewjackson@Andrews-MacBook-Pro.local>
2017-06-05 10:20:11 -0500

    updated views

--- spa/urls.py
+++ spa/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^about/', views.about, name='about'),
     url(r'^services/', views.services, name='services'),
+    url(r'^index/', views.index, name='index'),
 
 ]
 
