35b26ab415f7bdb1d8563793490a8e2700b383b7
Patrick Ryan <pjryan@my.okcu.edu>
2017-06-09 16:22:24 -0500

    added images and updated pages

--- spa/views.py
+++ spa/views.py
@@ -11,7 +11,25 @@ from django.http import HttpResponse
 
 def index(request):
 	template = loader.get_template('spa/index.html')
-	context = {}
+
+	title = "Facelifts"
+	desc = "This revolutionary treatment performed in four layers will change the image of your skin in just one application."
+	image = static("back.jpg")
+
+	title2 = "LED Treatment"
+	desc2 = "Increase new tissue growth, stimulate collagen production and reduce fine lines and wrinkles."
+	image2 = static("room.jpg")
+	image3 = static("head.jpg")
+	imageContent = getImageWithText(image)
+	imageContent2 = getImageWithText(image2)
+	imageContent3 = getImageWithText(image3)
+	text = getTitleBlock(title,desc)
+	content = getPhotoBlockComponent(image,"True",text)
+
+	text2 = getTitleBlock(title2,desc2)
+	content2 = getPhotoBlockComponent(image2,"True",text2)
+
+	context = {"photoBlock":imageContent,"photoBlock2":imageContent2,"photoBlock3":imageContent3}
 	return HttpResponse(template.render(context, request))
 def about(request):
 	template = loader.get_template('spa/about.html')
@@ -28,20 +46,31 @@ def peels(request):
 	title = "The Signature Facelift"
 	skinTypes = ["Rosacea","dull/tired","pregnant","dry/dehydrated","smoker’s complexion","post-microdermabrasion","post-surgery"]
 	peelTypes = ["Vitamin C","Enzyme Resurfacing Solution"]
-
-	content = getPhotoBlockComponent(image,imageOnLeftSide,description,title,skinTypes,peelTypes)
-	#contnet2 = getPhotoBlockComponent(image,"False",description,title)
+	text =  getTextBlock(description,title,skinTypes,peelTypes)
+	content = getPhotoBlockComponent(image,imageOnLeftSide,text)
 	image2 = static("a3.jpg")
-	content2 = getPhotoBlockComponent(image2,"False",description,title,skinTypes,peelTypes)
+	content2 = getPhotoBlockComponent(image2,"False",text)
 	image3 = static("a4.jpg")
-	content3 = getPhotoBlockComponent(image3,"True",description,title,skinTypes,peelTypes)
+	content3 = getPhotoBlockComponent(image3,"True",text)
 	image4 = static("a5.jpg")
-	content4 = getPhotoBlockComponent(image4,"False",description,title,skinTypes,peelTypes)
+	content4 = getPhotoBlockComponent(image4,"False",text)
 	context = {"photoBlock":[content,content2,content3,content4]}
 	template = loader.get_template('spa/services.html')
 	return HttpResponse(template.render(context, request))
-
-def getPhotoBlockComponent(image,imageOnLeftSide,desc,title,skinTypes,peelTypes):
-	imageContext = {"imagePath":image,"description":desc,"imageOnLeftSide":imageOnLeftSide,"title":title,"skinTypes":skinTypes,"peelTypes":peelTypes}
+def getPhotoBlockComponent(image,imageOnLeftSide,textComponent):
+	imageContext = {"imagePath":image,"imageOnLeftSide":imageOnLeftSide,"text":textComponent}
 	content = render_to_string('spa/imageTextBlock.html', imageContext)
 	return content
+def getTextBlock(desc,title,skinTypes,peelTypes):
+	imageContext = {"description":desc,"title":title,"skinTypes":skinTypes,"peelTypes":peelTypes}
+	content = render_to_string('spa/textbox.html', imageContext)
+	return content
+def getTitleBlock(title,desc):
+	imageContext = {"description":desc,"title":title}
+	content = render_to_string('spa/titleTextBox.html', imageContext)
+	return content
+def getImageWithText(image):
+	imageContext = {"imagePath":image}
+	content = render_to_string('spa/photoWithText.html', imageContext)
+	return content
+
